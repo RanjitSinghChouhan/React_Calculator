2fa035866594174f1a49ce2fd5485e39911c1223
RanjitSinghChouhan <ranjeet.singh.chouhan94@gmail.com>
2021-12-05 16:06:00 +0530

    message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Calculator from './Component/Calculator'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Calculator</h1>
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
